4da6483d9496e8e8475617ef2757eaf792f659a7
katelynarmstrong <katelynarmstrong6@gmail.com>
2022-01-08 11:39:02 -0600

    basics created

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// questions
+var questions = [
+    {
+        questionText: "Inside which HTML element do we put the JavaScript?",
+        options:["a. <scripting", "b. <javascript>", "c. <js>", "d. <script>"],
+        answer:"d. <script>"
+    },
+    {
+        questionText: "How would you write "Hello World" in an alert box?",
+        options:["a. msg('Hello World');", "b. alert('Hello World');", "c. msgBox('Hello World')", "d. alertBox('Hello World');"],
+        answer:"b. alert('Hello World');"
+    },
+    {
+        questionText: "How do you create a function in JavaScript?",
+        options:["a. function:myFunction()", "b. function=myFunction()", "c. function myFunction()"], "d. function:(myFunction)"],
+        answer:"b. function=myFunction"
+    },
+    {
+        questionText: "How can you add a comment in a JavaScript?",
+        options:["a. <!--This is a coment-->", "b. 'This is a comment", "c. //This is a comment", "d. /*This is a comment"],
+        answer:"c. //This is a comment"
+    },
+    {
+        questionText: "What will the following code return: Boolean(10 > 9)"
+        options:["a. false", "b. NaN", "c. true", "d. undefined"],
+        answer:"c. true"
+    },
+]
+
