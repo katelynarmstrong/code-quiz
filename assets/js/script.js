02381cf55c665c22fb12386b3ad6980c5f541fb8
katelynarmstrong <katelynarmstrong6@gmail.com>
2022-01-08 21:03:24 -0600

    scraped and pseudocoded js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// questions
+// questions for quiz
 var questions = [
     {
         questionText: "Inside which HTML element do we put the JavaScript?",
@@ -39,182 +39,40 @@ var startBtn = document.querySelector("#start-btn")
 let timerDisplay = document.querySelector("#timer")
 
 // function to hide quiz before user hits begin
-function hideQuestions(){
-    questionsEl.setAttribute("hidden", true)
-}
+
+// hide instructions and start button once quiz begins
 
 // start quiz and timer when user hits start button 
-startBtn.addEventListener("click", startQuiz);
-startBtn.addEventListener("click", timerCountdown);
 
 // function to start quiz with timer
-function startQuiz() {
-    // disable hidden feature
-    hideQuestions();
-    questionsEl.removeAttribute("hidden");
 
-    // initalize question
-    displayQuestion();
-
-    // start timer
-    intervalID = setInterval(timerCountdown, 1000);
-};
-
-// create a countdown function for the timer
-
-function timerCountdown(){
-    "time_remaining--;"
-    if (time_remaining < 0) {
-        endQuiz();
-    }
-    timerDisplay.textContent = time_remaining; 
-}
+// disable hidden feature
 
 // create function to display quiz questions 
 
-function displayQuestion(){
-    let question = questions[currentQuestion];
-    let answerChoice = question.choices;
-
-    let questionEl = document.querySelector("#question-text");
-    questionEl.textContent = question.questionText;
-
-    for (var i = 0; i < answer-choices.length; i++){
-        let answerChoice = answerChoices[i];
-        let answerBtn = document.querySelector("#choice"+i);
-        answerBtn.textContent = answerChoice;
-    }   
-}
-
 // when user clicks on answer, determine if its correct or incorrect
 
-document.querySelector("#answer-choices").addEventListener("click",checkAnswer);
+// correct answer increases the score 
 
+// incorrect answer decreases the time remaining 
 
-// determine if content for selected button and correct answer is the same 
-function correctAnswer(answerBtn){
-    return answerBtn.textContent === questions[currentQuestion].answer;
-}
+// go on to next question 
 
-// check to see if answer selected is correct
-function checkAnswer(event){
-    let answerBtn = event.target; 
-    // correct answer increases the score 
-    if (correctAnswer(answerBtn)){
-        score = score + 20;
-    }
-    // incorrect answer decreases the time remaining 
-    else {
-        if (time_remaining > 10){
-            time_remaining = time_remaining - 10;
-       }
-       else{
-           time_remaining = 0;
-           endQuiz();
-       }
-    }
-
-    // go on to next question 
-    currentQuestion++;
-
-    // if there are no more questions, end quiz
-    if (currentQuestion < questions.length){
-        displayQuestion();
-    }
-    else{
-        endQuiz();
-    }
-}
-
-document.querySelector("#answer-choices").addEventListener("click", checkAnswer)
-
-
-function endQuiz(){
-    let finalScore = document.querySelector("#score")
-    finalScore.textContent = "You scored " + score + " points!";
-    document.querySelector("#time-remaining").setAttribute("hidden", true);
-    document.querySelector("#question-text").setAttribute("hidden", true);
-    document.querySelector("#answer-choices").setAttribute("hidden", true);
-}
+// if there are no more questions, end quiz
 
 // when user hits submit button it adds their score and initials to the leaderboard
-var submitButton = document.querySelector("#initials-btn")
-
-submitButton.addEventListener("click", saveScore);
 
 // create function to save score of user
-function saveScore(event){
-    // disable refresh 
+
+// Ensure high score saves to see later 
     event.preventDefault();
 
-    // cannot have initals be null 
-    let initials = document.querySelector("#initials");
-    if (!initials.value){
-        alert("Please enter your initials.");
-        return;
-    }
+// add user to leaderboard for local storage
 
-    let highScores = {
-        initials: initials.value,
-        userScore: score
-    };
-
-    // add user to leaderboard for local storage
-    updateLeaderboard(highScores);
-
-    // hide questions because game is over 
-    hideQuestions();
-
-    // show the top scores
-    displayLeaderboard();
-}
+// hide questions once end of quiz activated 
 
 // update leaderboard in local storage
-function updateLeaderboard(highScores){
-    let leaderboard = getLeaderboard();
-    leaderboard.push(highScores);
-    localStorage.setItem("leaderboard", JSON.stringify(leaderboard));
-}
 
-// get leaderboard from local storage 
-function getLeaderboard(){
-    let storedLeaderboard = localStorage.getItem("leaderboard");
-    // if nothing in local storage add to existing 
-    if (storedLeaderboard !== null){
-        let leaderboard = JSON.parse(storedLeaderboard);
-        console.log(leaderboard)
-        return leaderboard;
-    }
-    else{
-        leaderboard = [];
-    }
-    return leaderboard;
-}
+// get leaderboard from local storage to review standing
 
-// display high scores from least to greatest
-function sortLeaderboard(){
-    let leaderboard = getLeaderboard();
-    if (!leaderboard){
-        return;
-    }
-
-    leaderboard.sort(function(a, b){
-        return b.userScore - a.userScore;
-    });
-
-    return leaderboard;
-}
-
-// 
-function displayLeaderboard(){
-    var leaderboardList = document.querySelector("#leaderboard-list");
-    leaderboardList.innerHTML = "";
-    let leaderboard = sortLeaderboard();
-    for (let i = 0; i < leaderboard.length; i++){
-        let userEntry = leaderboard[i];
-        let newScore = document.createElement("li");
-        newScore.textContent = 
-            userEntry.initials + " : " + userEntry.userScore;
-        leaderboardList.append(newScore);
-    }
-}
+// display high scores
