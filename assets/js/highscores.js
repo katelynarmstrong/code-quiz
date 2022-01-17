b6081f680892d2feba24c11b6baf874e1ab2067d
katelynarmstrong <katelynarmstrong6@gmail.com>
2022-01-16 19:45:07 -0600

    bugs fixed in highscores

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,10 +1,11 @@
 // get everything from in local storage, loop through it and then display it to the user
+var highscoresList = document.getElementById('highscores-list')
 var initials = localStorage.getItem('initials')
 var placeForInitialsOnHighScorePage = document.getElementById('initial-area')
 placeForInitialsOnHighScorePage.value = initials
 
 var highscoreList = document.createElement ("li");
-highscoreList.append(highscores-list)
+highscoreList.append('highscores-list')
 
 localStorage.getItem(scoreList);
 
